6df3eff13072429ae2df34c890d12b82e18b2a2b
khenneth <khennethlp@gmail.com>
2024-08-16 16:52:19 +0800

    to update

--- pages/superAdmin/plugins/js/functions.js
+++ pages/superAdmin/plugins/js/functions.js
@@ -1,105 +1,51 @@
-// const getData_masterlist = () => {
 
-//     $.ajax({
-//         type: "method",
-//         url: "../../process/superAdmin/load_data.php",
-//         data: {
-//             method: 'load_dataTable'
-//         },
-//         success: function (response) {
-//             document.getElementById('m_report_table').innerHTML = response;
-//         }
-//     });
-// }
+const get_details = (param) => {
+    var data = param;
 
-// document.addEventListener("DOMCONTENTLOADED", function(){
-// });
+    var serial_no = data;
+    console.log(param);
 
-const getData_masterlist = async () => {
-    const sortBy = document.getElementById("sortBy");
-    const getSerial = sessionStorage.getItem("setSerialNo");
-
-    try {
-        const response = await fetch("../../process/superAdmin/load_data.php", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: new URLSearchParams({
-                sortBy: sortBy.value,
-                serialNo: getSerial,
-            }),
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json(); // Parse the JSON response
-
-        // Update the HTML content of the table
-        document.getElementById("m_report_table").innerHTML = data.html;
-
-        // Update the batch number, group number, and training group fields
-        if (data.data.error) {
-            console.error(data.data.error);
-            alert(data.data.error);
-        } else {
-            document.getElementById("batchNo").value = data.data.batch_no || '';
-            document.getElementById("groupNo").value = data.data.group_no || '';
-            document.getElementById("training_group").value = data.data.training_group || '';
-        }
-
-        // Get the serial number links and set up event listeners
-        const viewLinks = document.querySelectorAll("a[data-serial]");
-        viewLinks.forEach(function (link) {
-            link.addEventListener("click", function (event) {
-                event.preventDefault(); // Prevent the default action (e.g., following the link)
-
-                const serialNumber = this.getAttribute("data-serial"); // Get the data-serial attribute value
-
-                document.getElementById("serialNo").value = serialNumber;
-                console.log("Serial Number:", serialNumber); // Log it to the console
-                sessionStorage.setItem("setSerialNo", serialNumber); // Set to session storage
-            });
-        });
-
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-};
-// const get_training_record = async () => {
-//     var getSerial = sessionStorage.getItem('setSerialNo');
-
-//     console.log('Serial Number from Session Storage:', getSerial);
-//     try {
-//         const response = await fetch('../../process/superAdmin/load_data.php', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded'
-//             },
-//             body: new URLSearchParams({
-//                 // method: 'load_dataTable',
-//                 serialNo: getSerial,
-//             })
-//         });
-
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-
-//         const data = await response.json();
-//         document.getElementById('batchNo').value = data.batch_no;
-//         document.getElementById('groupNo').value = data.group_no;
-//         document.getElementById('training_group').value = data.training_group;
-
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//     }
-// }
+    $('#serialNo').val(serial_no);
+    sessionStorage.setItem("setSerialNo", serial_no)
+}
 
 document.getElementById("sortBy").addEventListener("change", () => {
-  getData_masterlist(); // Fetch the data when sortBy changes
-});
+    getData_masterlist(); // Fetch the data when sortBy changes
+  });
+
+const getData_masterlist = () => {
+//   var batchNo = document.getElementById("batchNo").value;
+  var sortBy = document.getElementById("sortBy").value;
+  const getSerial = sessionStorage.getItem("setSerialNo");
+
+  $.ajax({
+    type: "POST",
+    url: "../../process/superAdmin/load_data.php",
+    data: {
+      method: "getData_masterlist",
+      sortBy: sortBy,
+      serial_no: getSerial
+    },
+    success: function (response) {
+      document.getElementById("myDataTable").innerHTML = response;
+    },
+  });
+};
+
+// const data_modal = () => {
+//   const getSerial = sessionStorage.getItem("setSerialNo");
+
+//   $.ajax({
+//     type: "POST",
+//     url: "../../process/superAdmin/load_data.php",
+//     data: {
+//       method: "getData_masterlist",
+//       serial_no: getSerial
+//     },
+//     success: function (response) {
+//       document.getElementById("myDataTable").innerHTML = response;
+//     },
+//   });
+// };
 
 getData_masterlist();
--- process/superAdmin/load_data.php
+++ process/superAdmin/load_data.php
@@ -1,63 +1,43 @@
 <?php
 require '../../process/conn.php';
 
+$method = $_POST['method'];
 
-// Database connection
-// $conn = new PDO('mysql:host=hostname;dbname=database', 'username', 'password');
-$sortBy = intval($_POST['sortBy'] ?? 0);
-$serialNo = $_POST['serialNo'] ?? '';
+if($method == 'getData_masterlist'){
 
-// Initialize output
-$html = '';
-$data = [
-    'batch_no' => '',
-    'group_no' => '',
-    'training_group' => '',
-    'error' => ''
-];
+    $sortBy = intval($_POST['sortBy'] ?? 0);
+    $serialNo = isset($_POST['serialNo']) ? $_POST['serialNo'] : '';
 
-// Fetch data from t_upload_file
-$sql = 'SELECT * FROM t_upload_file LIMIT :sortBy';
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':sortBy', $sortBy, PDO::PARAM_INT);
-$stmt->execute();
-
-$c = 0;
-if ($stmt->rowCount() > 0) {
-    foreach ($stmt as $k) {
-        $c++;
-        $html .= '<tr>';
-        $html .= '<td>' . $c . '</td>';
-        $html .= '<td>' . $k['serial_no'] . '</td>';
-        $html .= '<td>' . (strlen($k['file_name']) > 50 ? substr($k['file_name'], 0, 50) . '...' : $k['file_name']) . '</td>';
-        $html .= '<td><a href="" data-toggle="modal" data-target="#super_record_m" data-serial="' . $k['serial_no'] . '">View</a></td>';
-        $html .= '</tr>';
-    }
-} else {
-    $html .= '<tr><td>No data.</td></tr>';
-}
-
-// Fetch data from t_training_record if serialNo is set
-if ($serialNo) {
-    $sql = 'SELECT * FROM t_training_record WHERE serial_no = :serialNo';
+    $sql = 'SELECT * FROM t_training_record where serial_no = :serial_no';
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':serialNo', $serialNo, PDO::PARAM_STR);
+    $stmt->bindParam(':serial_no', $serialNo, PDO::PARAM_INT);
     $stmt->execute();
 
-    $k = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // foreach($res as $k){
 
-    if ($k !== false) {
-        $data['batch_no'] = $k['batch_no'];
-        $data['group_no'] = $k['group_no'];
-        $data['training_group'] = $k['training_group'];
+    //     $batchNo = $k['batch_no'];
+    // }
+    
+    $sql = 'SELECT * FROM t_upload_file LIMIT :sortBy';
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':sortBy', $sortBy, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    $c = 0;
+    if ($stmt->rowCount() > 0) {
+        foreach ($stmt as $k) {
+            $c++;
+            echo '<tr>';
+            echo '<td>' . $c . '</td>';
+            echo '<td>' . $k['serial_no'] . '</td>';
+            echo '<td>' . (strlen($k['file_name']) > 50 ? substr($k['file_name'], 0, 50) . '...' : $k['file_name']) . '</td>';
+            echo '<td><a href="" data-toggle="modal" data-target="#super_record_m" onclick=get_details("' . $k['serial_no'] . '");>View</a></td>';
+            echo '</tr>';
+        }
     } else {
-        $data['error'] = 'No record found with the provided serial number.';
-    }
+        echo '<tr><td>No data.</td></tr>';
+    }    
 }
 
-// Output both HTML and additional data as JSON
-echo json_encode([
-    'html' => $html,
-    'data' => $data
-]);
 ?>
